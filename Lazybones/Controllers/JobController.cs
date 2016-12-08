7f6c72150e10f3a40ac33fc63653a309847c10e6
Tom Nagle <tnagle10@gmail.com>
2016-12-08 10:08:30 -0500

    Got job/create method working

---
+++ Lazybones/Controllers/JobController.cs
@@ -0,0 +1,35 @@
+﻿using Lazybones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lazybones.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Lazybones.Models.Job job)
+        {
+            // Creates a database reference to the user db
+            LazinessSolutionsEntities3 jobDB = new LazinessSolutionsEntities3();
+            // Add the job passed to create post
+            jobDB.Jobs.Add(job);
+            // Save changes
+            jobDB.SaveChanges();
+
+            return RedirectToAction("Index", "Job");
+        }
+    }
+}
