92fe0c64070cbe675f0209a841dea3ddcab5f4c7
Aleena Tucker <thumbaleena@outlook.com>
2016-12-09 16:56:42 -0500

    Search works again!

--- Lazybones/Models/Job.cs
+++ Lazybones/Models/Job.cs
@@ -17,23 +17,20 @@ namespace Lazybones.Models
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        [Display(Name = "Starts")]
+        [Display(Name = "Start")]
         public Nullable<System.DateTime> Start_Time_Date { get; set; }
-        [Display(Name = "Expires")]
+        [Display(Name = "Ends")]
         public Nullable<System.DateTime> Expirey_Time_Date { get; set; }
         public string Category { get; set; }
-        [Display(Name = "Flat rate pay")]
         public Nullable<decimal> Pay { get; set; }
-        [Display(Name = "Or best bid")]
         public bool Best_Bid { get; set; }
         public string Picture_Location { get; set; }
-        [Display(Name = "Date completed")]
+        [Display(Name = "Date Completed")]
         public Nullable<System.DateTime> Date_Completed { get; set; }
         public string Poster_Name { get; set; }
         public string Getter_Name { get; set; }
         public string Status { get; set; }
         public int ID { get; set; }
-        [Display(Name = "Payment status")]
         public string Payment_Status { get; set; }
         [Display(Name = "Call")]
         public bool Contact_By_Phone { get; set; }
@@ -47,7 +44,6 @@ namespace Lazybones.Models
         public string State { get; set; }
         public string Getter { get; set; }
         public string Poster { get; set; }
-        [Display(Name = "Current bid amount")]
         public Nullable<decimal> Bid_Amount { get; set; }
         [Display(Name = "Gig location same as home?")]
         public bool Same_as_Home { get; set; }
