4be07c01fdbe4c7517fa15419445ced38a315b42
Aleena Tucker <thumbaleena@outlook.com>
2016-12-10 14:33:46 -0500

    updated DB to add bools for userprofile contact methods, updated models, controllers, and views

--- Lazybones/Models/AspNetUser.cs
+++ Lazybones/Models/AspNetUser.cs
@@ -33,8 +33,10 @@ namespace Lazybones.Models
         public bool Go_Getter { get; set; }
         public string Last_Name { get; set; }
         public string Mobile_Phone { get; set; }
-        public string Preferred_Contact_Method { get; set; }
         public string State { get; set; }
         public string Zip { get; set; }
+        public bool ByCall { get; set; }
+        public bool ByText { get; set; }
+        public bool ByEmail { get; set; }
     }
 }
